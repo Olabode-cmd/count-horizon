c169a6d4097cbbddbb55d1e362bf52c7ffd5eef5
Olabode-cmd <olabodebalogun80@gmail.com>
2024-06-18 17:27:52 +0100

    overflow

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   basePath: '',  // Remove the 'horizon-ui-chakra' or set it to an empty string
 };
 
